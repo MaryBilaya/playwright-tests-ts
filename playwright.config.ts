50a0a914eeac25b15b7ac026f4a634bc0e3f0034
Maryia Bilaya <m.bilaya@a1.by>
2026-01-04 23:42:03 +0300

    Homework_23: tasks 1, 2, 3

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './test-browser-actions',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -30,6 +30,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    screenshot: 'on',
     trace: 'off'   //retain-on-failure — сохраняет trace только на фейле, on-first-retry — идеален для CI, on — всегда (не рекомендую для CI)
   },
   timeout: 60_000, //добавляла вручную
